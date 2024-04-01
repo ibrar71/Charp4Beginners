e0c7ded77f76fd5a34a97b6b360c73fa161d0a24
ibrar71 <ibrar.se@gmail.com>
2024-04-01 15:00:45 +0200

    C# Learning template

---
+++ Charp4Beginners/Part_060/Class060.cs
@@ -0,0 +1,12 @@
+//Can be used to save current lecture  
+ 
+using System; 
+  
+namespace Charp4Beginners 
+{ 
+    internal Class060 
+    { 
+        //Code goes here for the current lesson 
+    } 
+}	 
+  
